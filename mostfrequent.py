8bfef87a87bd747d1659a98418886fa4d338ffd5
arinparashar <130351898+arinparashar@users.noreply.github.com>
2023-07-03 00:14:28 +0530

    Update mostfrequent.py

--- mostfrequent.py
+++ mostfrequent.py
@@ -1,15 +1,15 @@
 def most_frequent(string):
-    # letter frequencies will be stored here
+    
     freq_dict = {}
 
-    # frequency of each letter is counted
+    
     for letter in string:
         freq_dict[letter] = freq_dict.get(letter, 0) + 1
 
-    # dictionary is sorted in descneding order
+   
     sorted_freq = sorted(freq_dict.items(), key=lambda x: x[1], reverse=True)
 
-    # finally the letter adn their frequencies are printed
+    
     for letter, frequency in sorted_freq:
         print(f"{letter} = {frequency:02d}")
 
