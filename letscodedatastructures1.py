72081a384a28ca2fbdd8b22240c2e24687f2096c
arinparashar <130351898+arinparashar@users.noreply.github.com>
2023-07-05 02:18:29 +0530

    Update letscodedatastructures1.py

--- letscodedatastructures1.py
+++ letscodedatastructures1.py
@@ -1,9 +1,10 @@
+#radius of circle
 import math
 radius = float(input("Input the radius of the circle: "))
 area = math.pi * radius ** 2
 print(f"The area of the circle with radius {radius} is: {area}")
 
-
+#extension
 filename = input("Input the Filename: ")
 extension = filename.split(".")[-1]
 print(f"The extension of the file is: '{extension}'")
